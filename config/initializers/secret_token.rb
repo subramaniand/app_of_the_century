f772a93a44e9a87790c78999407689818c78657b
subramaniand <subramaniand@aditi.com>
2013-06-24 23:33:22 +0000

    Initial commit on azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppOfTheCentury::Application.config.secret_token = 'cc1318e302dfc144af2ac8edce843da72bbb86d86fef5849c899837faffe9d11be7e372df5ac1215086ca5f7a6c6a6f128e6e4dd4d0e1d8320ca492eacf45132'
